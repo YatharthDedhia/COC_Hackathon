08e7eed0c6a52cd071893769639bc39e6222c4a8
YatharthDedhia <dedhiayatharth2004@gmail.com>
2023-01-13 01:06:10 +0530

    Added File structure

---
+++ hackathon/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+	Landing,
+	Dashboard,
+	Profile,
+	Admin,
+	Test,
+	Opening,
+	Institute
+} from './pages';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+const App = () => {
+	return (
+		<div className="App">
+			{/* <React.StrictMode> */}
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<Opening />} />
+					<Route exact path="/home" element={<Home />} />
+					<Route exact path="/profile" element={<Profile />} />
+					<Route exact path="/leave" element={<Leave />} />
+					<Route exact path="/guest" element={<Guest />} />
+					<Route exact path="/opening" element={<Landing />} />
+				</Routes>
+			</BrowserRouter>
+			{/* </React.StrictMode> */}
+		</div>
+	);
+};
+
+export default App;
