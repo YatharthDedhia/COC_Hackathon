9ac77b55e54c70e6485d321fd8b85b2f0e2d2a08
YatharthDedhia <dedhiayatharth2004@gmail.com>
2023-01-13 01:30:00 +0530

    removed routing bug

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import {
-	Landing,
-	Dashboard,
-	Profile,
-	Admin,
-	Test,
-	Opening,
-	Institute
-} from './pages';
+  Opening
+  // Home,
+  // Profile,
+  // Leave,
+  // Guest,
+  // Landing
+} from './pages'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
-	return (
-		<div className="App">
-			{/* <React.StrictMode> */}
-			<BrowserRouter>
-				<Routes>
-					<Route exact path="/" element={<Opening />} />
-					<Route exact path="/home" element={<Home />} />
-					<Route exact path="/profile" element={<Profile />} />
-					<Route exact path="/leave" element={<Leave />} />
-					<Route exact path="/guest" element={<Guest />} />
-					<Route exact path="/opening" element={<Landing />} />
-				</Routes>
-			</BrowserRouter>
-			{/* </React.StrictMode> */}
-		</div>
-	);
+  return (
+    <div className="App">
+      {/* <React.StrictMode> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Opening />} />
+          {/* <Route exact path="/home" element={<Home />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/leave" element={<Leave />} />
+          <Route exact path="/guest" element={<Guest />} />
+          <Route exact path="/landing" element={<Landing />} /> */}
+        </Routes>
+      </BrowserRouter>
+      {/* </React.StrictMode> */}
+    </div>
+  );
 };
 
 export default App;
---
+++ hackathon/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Opening } from './opening/Opening';
+// export { default as Home } from './home/Home';
+// export { default as Profile } from './profile/Profile';
+// export { default as Leave } from './leave/Leave';
+// export { default as Guest } from './guest/Guest';
+// export { default as landing } from './landing/Landing';
