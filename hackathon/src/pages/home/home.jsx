e426e62c3a1654a538292efe5031924692e690ad
YatharthDedhia <dedhiayatharth2004@gmail.com>
2023-01-13 13:26:23 +0530

    Added Menu

--- hackathon/src/pages/home/home.jsx
+++ hackathon/src/pages/home/home.jsx
@@ -1,61 +1,136 @@
 import React, { useState, useEffect } from 'react';
 
-import './css/dashboard.css';
-import './css/copylink.css';
-import './css/commoninput.css';
-import './css/search.css'
-import './css/table.css'
-import './css/sidebar.css'
-import './css/calendar.css'
+import './css/navbar.css';
+import './css/menu.css';
 
-
-const NavLinks = () => (
-    <React.Fragment>
-        <p>
-            <div>
-                <form action="" autocomplete="on">
-                    <input className="Search1" type="text" id='search' name='search' placeholder="Search" />
-                    <input id="search_submit" value="Rechercher" type="submit" />
-                </form>
-            </div>
-        </p>
-        <p>
-            <a href="/profile">Profile</a>
-        </p>
-    </React.Fragment>
-);
-
-const Sidebar = () => {
+const Menu = () => {
     return (
-        <nav role="navigation">
-            <div id="menuToggle">
-                <input type="checkbox" />
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-md-12">
+                    <div class="header">
 
-                <span></span>
-                <span></span>
-                <span></span>
-                <ul id="menu">
-                    <a href="#"><li>Announcements</li></a>
-                    <a href="#"><li>Timetable</li></a>
-                    <a href="#"><li>Test Schedule</li></a>
-                    <a href="#"><li>Homework</li></a>
-                </ul>
+                        <h1 class="heading-1">OUR MENU</h1>
+                        <hr />
+                    </div>
+                </div>
             </div>
-        </nav>
-    );
-};
-
-const Navbar = () => {
-    return (
-        <div className="landing-navbar">
-            {/* m */}
-            <Sidebar />
-            {/* m */}
-            <div className="landing-navbar-logo">
-                <h1>Test Platform</h1>
-            </div>
-            <div className="landing-navbar-links">
-                <NavLinks />
+            <div class="clear-food">
+                <div class="menu-food">
+                    <ul class="menu-food-pennal"></ul>
+                    <div class="food-list">
+                        <div class="list-1">
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-1.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-2.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-3.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-4.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-5.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-6.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-7.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-8.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3-mon-1">
+                                <div class="menu-list-1">
+                                    <div class="pull-left food-img-1">
+                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-9.png" alt="" />
+                                    </div>
+                                    <div class="dish-name">
+                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
+                                        <p>Mushroom / Garlic / Veggies</p>
+                                        <div class="dishs-price"><span>$12</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
@@ -63,9 +138,29 @@ const Navbar = () => {
 
 const Home = () => {
     return (
-        <React.Fragment>
-            <Navbar />
-        </React.Fragment>
+        <div>
+            <nav class="nav">
+                <div class="nav-main">
+                    <div class="logo">Hostel<span>Mess</span></div>
+                    <ul class="nav-links">
+                        <li class="nav-link dropdown"><a href="" class="dropdown">Specials <i
+                            class="bi bi-chevron-compact-down"></i></a>
+                            <ul class="dropdown-list">
+                                <li class="nav-link"><a href="">Basundi</a>
+                                    <li class="nav-link">
+                                        <a href="">Brownie</a>
+                                    </li>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-link"><a href="/guest">Add Guest</a></li>
+                        <li class="nav-link"><a href="/leave">Apply Leave</a></li>
+                        <li class="nav-link"><a href="/profile">Profile</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <Menu />
+        </div>
     );
 };
 
