241b8929c60efe103680525a7b86a02b5c2a48e2
YatharthDedhia <dedhiayatharth2004@gmail.com>
2023-01-13 17:01:27 +0530

    API integration homepage

--- hackathon/src/pages/home/home.jsx
+++ hackathon/src/pages/home/home.jsx
@@ -3,131 +3,60 @@ import React, { useState, useEffect } from 'react';
 import './css/navbar.css';
 import './css/menu.css';
 
+const menu = [
+    {
+        type: 'Lunch',
+        item: 'Paneer',
+        date: '26/11/2022',
+        ingredients: 'Mushroom / Garlic / Veggies'
+    },
+    {
+        type: 'Dinner',
+        item: 'Chole',
+        date: '21/11/2022',
+        ingredients: 'Mushroom / Garlic / Veggies'
+    },
+    {
+        type: 'Breakfast',
+        item: 'Bread-Butter',
+        date: '22/11/2022',
+        ingredients: 'Mushroom / Garlic / Veggies'
+    }
+];
+
+const MenuPrint = () => {
+    return (
+        menu.map((food) => (
+            <div class="col-3-mon-1">
+                <div class="menu-list-1">
+                    <div class="dish-name">
+                        <p>{food.date}</p>
+                        <h4>{food.item}</h4>
+                        <p>{food.type}</p>
+                        <p className='ingredients'>{food.ingredients}</p>
+                    </div>
+                </div>
+            </div>
+        ))
+    )
+}
+
 const Menu = () => {
     return (
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-md-12">
                     <div class="header">
-
                         <h1 class="heading-1">OUR MENU</h1>
-                        <hr />
                     </div>
                 </div>
             </div>
             <div class="clear-food">
                 <div class="menu-food">
-                    <ul class="menu-food-pennal"></ul>
+                    <ul class="menu-food-pennal" style={{ height: 43 + 'px' }}></ul>
                     <div class="food-list">
                         <div class="list-1">
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-1.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-2.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-3.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-4.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-5.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-6.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-7.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-8.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3-mon-1">
-                                <div class="menu-list-1">
-                                    <div class="pull-left food-img-1">
-                                        <img class="hinh-1" src="https://www.multipurposethemes.com/html-templates/morsel/images/menu-dish/dish-9.png" alt="" />
-                                    </div>
-                                    <div class="dish-name">
-                                        <h4> ITALIAN SOURCE MUSHROOM</h4>
-                                        <p>Mushroom / Garlic / Veggies</p>
-                                        <div class="dishs-price"><span>$12</span></div>
-                                    </div>
-                                </div>
-                            </div>
+                            <MenuPrint />
                         </div>
                     </div>
                 </div>
