679a15b3f072835016f9a96c99f1e66465e5da80
manav2580 <manavshah.2003.ms@gmail.com>
2023-01-14 02:15:07 +0530

    merged code

--- hackathon/src/pages/index.js
+++ hackathon/src/pages/index.js
@@ -1,7 +1,7 @@
 export { default as Opening } from './opening/Opening';
 export { default as Home } from './home/home';
 export { default as Profile } from './profile/Profile';
-export { default as Leave } from './leave/Leave';
+export { default as Leave } from './leave/leave';
 // export { default as Guest } from './guest/Guest';
 export { default as Landing } from './landing/landing';
 export { default as Admin } from './admin/admin';
