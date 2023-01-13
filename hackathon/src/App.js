d5e1c39d76ab514c34bf6ec186b0945a9171cde8
YatharthDedhia <dedhiayatharth2004@gmail.com>
2023-01-13 13:29:25 +0530

    Added Opening page

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
   Opening,
-  Home,
-  Profile,
+  Home
+  // Profile,
   // Leave,
   // Guest,
-  Landing
+  // Landing
 } from './pages'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -18,10 +18,10 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Opening />} />
           <Route exact path="/home" element={<Home />} />
-           <Route exact path="/profile" element={<Profile />} />
-          {/*<Route exact path="/leave" element={<Leave />} />
-          <Route exact path="/guest" element={<Guest />} />*/
-          <Route exact path="/landing" element={<Landing />} /> }
+          {/* <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/leave" element={<Leave />} />
+          <Route exact path="/guest" element={<Guest />} />
+          <Route exact path="/landing" element={<Landing />} /> */}
         </Routes>
       </BrowserRouter>
       {/* </React.StrictMode> */}
