80f28396f05ae531268d14970e2c77d73d2b2aa5
manav2580 <manavshah.2003.ms@gmail.com>
2023-01-13 14:52:19 +0530

    4 pages done

--- hackathon/src/pages/index.js
+++ hackathon/src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Opening } from './opening/Opening';
 export { default as Home } from './home/home';
-// export { default as Profile } from './profile/Profile';
+export { default as Profile } from './profile/Profile';
 // export { default as Leave } from './leave/Leave';
 // export { default as Guest } from './guest/Guest';
-// export { default as Landing } from './landing/Landing';
+export { default as Landing } from './landing/Landing';
